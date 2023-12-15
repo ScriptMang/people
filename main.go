ab58f567949bd4c3152ef3c7fda595ead3ddeda0
Andy Peralta <aperalta686@gmail.com>
2023-12-15 17:07:20 -0500

    test json redirect
    created a json file with fake data to redirect to a json url

--- main.go
+++ main.go
@@ -6,28 +6,15 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type person struct {
-	ID    string `json:"id"`
-	Fname string `json:"firstname"`
-	Lname string `json:"lastname"`
-	Age   int    `json:"age"`
-}
-
-var persons = []person{
-	{ID: "1", Fname: "Bob", Lname: "Ross", Age: 50},
-	{ID: "2", Fname: "Kongary", Lname: "Kat", Age: 34},
-	{ID: "3", Fname: "LimeStone", Lname: "Rose", Age: 45},
-}
-
 func getPersons(c *gin.Context) {
-	c.JSON(http.StatusOK, persons)
+	c.Redirect(http.StatusMovedPermanently, "/docs/persons.json")
 }
 
 func main() {
 	router := gin.Default()
 	router.LoadHTMLGlob("docs/*.html")
 
-	router.Static("/css", "./docs/css/")
+	router.Static("/docs", "./docs")
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", gin.H{
 			"content": "This is an index page...",
