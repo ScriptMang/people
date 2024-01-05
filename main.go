b7484d7e0d1b27d4803e4be98cb2fa2f2da8b482
Andy Peralta <aperalta686@gmail.com>
2024-01-05 00:53:29 -0500

    Refactor code by using template files
     - simplify routing through proper use of html tempaltes
     - 'index' template works as intended, with proper css styling
     - 'request1' template  doesn't display the test file 'persons.json' file yet

--- main.go
+++ main.go
@@ -1,25 +1,32 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func getPersons(c *gin.Context) {
-	c.Redirect(http.StatusMovedPermanently, "./persons.json")
+	c.HTML(http.StatusOK, "request1.tmpl", gin.H{
+		"people": "persons.json",
+	})
 }
 
 func main() {
 	router := gin.Default()
-	router.LoadHTMLGlob("docs/*.html")
+	router.LoadHTMLGlob("docs/templates/*")
 
 	router.Static("docs", "./docs")
 	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", gin.H{
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
 			"content": "This is an index page...",
 		})
 	})
-	router.GET("/persons", getPersons)
-	router.Run()
+	router.GET("/request1", getPersons)
+	err := router.Run("127.0.0.1:8080")
+	if err == nil {
+		fmt.Printf("Route couldn't be ran, %v\n", err)
+	}
+
 }
